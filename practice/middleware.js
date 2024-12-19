f33547f0a6fe865bb7cd007c5f14c5fadeb4dc6b
Isha Viragi <isha.viragi@gmail.com>
2024-12-19 13:54:38 -0500

    Create middleware to generate random rateOfSucess and miliseconds

---
+++ practice/middleware.js
@@ -0,0 +1,8 @@
+export function rateOfSuccessCalculator() {
+  return Math.ceil(Math.random() * 100);
+};
+
+export function randomMillisecGenerator() {
+  return Math.ceil(Math.random() * 4000);
+}
+
