e4dc06d93ecc1f1afa3bf39f653a5f2ceba3109d
Isha Viragi <isha.viragi@gmail.com>
2024-12-19 17:56:56 -0500

    Practice Promise chaining by simulating fetching posts based on userID

---
+++ practice/practice6.js
@@ -0,0 +1,66 @@
+//Practice Promise Chaining
+
+import { rateOfSuccessCalculator, randomMillisecGenerator } from "./middleware.js"
+
+const c6 = document.querySelector('.js-container6');
+c6.innerHTML = 'Loading...'
+
+//Instagram Simulation
+
+//First we need to fetch user
+
+function fetchUser(userID) {
+
+  return new Promise((resolve, reject) => {
+
+    const noUserRate = 30;
+    const rateOfSuccess = rateOfSuccessCalculator();
+    const milisecs = randomMillisecGenerator();
+
+    setTimeout(() => {
+      if (rateOfSuccess > noUserRate) {
+        c6.innerHTML = `<p>Found User with ID ${userID}</p>`;
+        resolve(userID); //Return the post ids
+      }
+      else if (rateOfSuccess <= noUserRate) reject(`No user with ID '${userID}' found`);
+    }, milisecs);
+  });
+};
+
+//If the user is found, we would chain
+//And search in the 'posts' table by
+//Utilizing 'normalization' and 
+//Referencing the user ID (Foreign key) to get the posts we are looking for 
+//(Given that we are using a Relational Database)
+
+function fetchPost(userID) {
+
+  return new Promise((resolve, reject) => {
+
+    const noPostsRate = 30;
+    const rateOfSuccess = rateOfSuccessCalculator();
+    const milisecs = randomMillisecGenerator();
+
+    setTimeout(() => {
+      if (rateOfSuccess > noPostsRate) {
+        c6.innerHTML += `<p>Found posts 1, 2, 3 for userID ${userID}</p>`
+        resolve('Post1, Post 2, Post 3');  //Return the latest post
+      }
+      else reject('User has no posts');
+    }, milisecs);
+  });
+};
+
+
+function displayPost(posts) {
+  c6.innerHTML += `<p>Now Displaying: ${posts}</p>`;
+}
+
+fetchUser(1)
+  .then(result => fetchPost(result))
+  .then(result => displayPost(result))
+  .catch(error => {
+    if (c6.innerHTML === 'Loading...')
+      c6.innerHTML = `<p>Error: ${error}</p>`;
+    else c6.innerHTML += `<p>Error: ${error}</p>`;
+  })
