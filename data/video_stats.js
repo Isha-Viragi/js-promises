a6f0ea073f6e95e577378cef51663cd0747398d4
Isha Viragi <isha.viragi@gmail.com>
2024-12-20 12:31:34 -0500

    Create mock video stats data

---
+++ data/video_stats.js
@@ -0,0 +1,20 @@
+export const video_stats = [
+  {
+    video_id: 1,
+    likes: '18M',
+    dislikes: '150k',
+    views: '1.6B'
+  },
+  {
+    video_id: 2,
+    likes: '18k',
+    dislikes: '1k',
+    views: '2.8M'
+  },
+  {
+    video_id: 3,
+    likes: '700k',
+    dislikes: '10',
+    views: '13M'
+  }
+]
