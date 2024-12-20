dae52755869cc309917a6f02e46018a946467830
Isha Viragi <isha.viragi@gmail.com>
2024-12-20 13:42:18 -0500

    Create middleware functions to simulate network/server errors

--- middleware.js
+++ middleware.js
@@ -2,4 +2,19 @@ export function generateRandomMilisecs() {
   return Math.ceil(Math.random() * 4000);
 }
 
+const chancesOfNetworkError = 30
+const chancesOfServerError = 30
 
+function simulateGenericError(chancesOfError) {
+  const randomChance = Math.ceil(Math.random() * 100);
+  if (randomChance > chancesOfError) return false;
+  else return true;
+}
+
+export function simulateNetworkError() {
+  return simulateGenericError(chancesOfNetworkError);
+}
+
+export function simulateServerError() {
+  return simulateGenericError(chancesOfServerError);
+}
