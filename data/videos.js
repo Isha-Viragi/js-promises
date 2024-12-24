d68cef482e3ddb4328e3ded5fb248f9a52c2d084
Isha Viragi <isha.viragi@gmail.com>
2024-12-24 13:09:56 -0500

    Use built in class Date to store date object for uploadDate property

--- data/videos.js
+++ data/videos.js
@@ -4,7 +4,7 @@ export const videos = [
     videoUrl: '../assets/videos/rick.png',
     title: 'Rick Astley - Never Gonna Give You Up',
     description: `“Never Gonna Give You Up” was a global smash on its release in July 1987, topping the charts in 25 countries including Rick’s native UK and the US Billboard Hot 100.  It also won the Brit Award for Best single in 1988. Stock Aitken and Waterman wrote and produced the track which was the lead-off single and lead track from Rick’s debut LP “Whenever You Need Somebody”.  The album was itself a UK number one and would go on to sell over 15 million copies worldwide.`,
-    uploadDate: 'Oct 25, 2009',
+    uploadDate: new Date,
     creatorId: 1
   },
 
@@ -14,7 +14,7 @@ export const videos = [
     title: 'Cutest Cat Videos - Compilation',
     description: `Welcome to the Pets Island where you can find lots of cute, funny and adorable videos about Pets. If you are an animal lover, you are in the right place. Because we will bring you daily videos of cutest Pets in the world. Are you ready for that?
     Make sure to subscribe and hit the bell for daily updates. Pets Island is always waiting for you.`,
-    uploadDate: 'Jun 30, 2020',
+    uploadDate: new Date,
     creatorId: 2
   },
 
@@ -23,7 +23,7 @@ export const videos = [
     videoUrl: '../assets/videos/kurzgesagt.jpg',
     title: '4.5 Billion Years in 1 Hour',
     description: `Earth is 4.5 billion years old - which is approximately the same amount of time it took us to create this video. We’ve scaled the complete timeline of our Earth’s life into our first animated movie! Every second shows about a million years of the planet’s evolution. Hop on a musical train ride and experience how long a billion years really is. It’s the perfect background for your next party, a great way to take a break from studying, or a fascinating companion while you’re on the go … and our celebration of 10 years of kurzgesagt.`,
-    uploadDate: 'Nov 10, 2023',
+    uploadDate: new Date,
     creatorId: 3
   },
 
