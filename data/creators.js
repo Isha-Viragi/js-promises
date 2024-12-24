507c6b4480d7764e52c7e7e7fcb693ba2090dc61
Isha Viragi <isha.viragi@gmail.com>
2024-12-24 13:11:29 -0500

    Create creators data file

---
+++ data/creators.js
@@ -0,0 +1,20 @@
+export const creators = [
+  {
+    creatorId: 1,
+    creatorName: 'Rick Astley',
+    subscriberCount: '4.21M',
+    videoCount: '287'
+  },
+  {
+    creatorId: 2,
+    creatorName: 'Pets Island',
+    subscriberCount: '18.7K',
+    videoCount: '333'
+  },
+  {
+    creatorId: 3,
+    creatorName: 'Kurzgesagt',
+    subscriberCount: '23.4M',
+    videoCount: '264'
+  }
+]
