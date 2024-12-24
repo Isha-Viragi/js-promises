c8b5cb309f2b7665efe160edfbd36ec223d7aa56
Isha Viragi <isha.viragi@gmail.com>
2024-12-24 13:10:59 -0500

    Create trendingVideos data file

---
+++ data/trendingVideos.js
@@ -0,0 +1,24 @@
+export const trendingVideos = [
+  {
+    creatorId: 1,
+    videoId: 1,
+    category: 'music',
+    categoryRank: 1,
+    overallRank: 3
+  },
+  {
+    creatorId: 2,
+    videoId: 1,
+    category: 'cute',
+    categoryRank: 1,
+    overallRank: 1
+  },
+  {
+    creatorId: 3,
+    videoId: 1,
+    category: 'education',
+    categoryRank: 1,
+    overallRank: 2
+  }
+
+]
