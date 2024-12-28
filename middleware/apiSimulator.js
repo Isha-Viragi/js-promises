0cff2c5517be65774507060dc0262a98907dc6a9
Isha Viragi <isha.viragi@gmail.com>
2024-12-28 17:49:09 -0500

    Change file name from middleware.js to apiSimulator.js and move to middleware folder

--- middleware/apiSimulator.js
+++ middleware/apiSimulator.js
@@ -1,9 +1,9 @@
 export function generateRandomMilisecs() {
-  return Math.ceil(Math.random() * 4000);
+  return Math.ceil(Math.random() * 1000);
 }
 
-const chancesOfNetworkError = 10
-const chancesOfServerError = 10
+const chancesOfNetworkError = 0
+const chancesOfServerError = 0
 
 function simulateGenericError(chancesOfError) {
   const randomChance = Math.ceil(Math.random() * 100);
