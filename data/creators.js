3ca8c80f90d4d96b82f445c5b1fd764e89a7a2da
Isha Viragi <isha.viragi@gmail.com>
2024-12-28 17:48:16 -0500

    Add profile pictures of creator accounts to assets/profiles/creators

--- data/creators.js
+++ data/creators.js
@@ -1,18 +1,21 @@
 export const creators = [
   {
     creatorId: 1,
+    creatorProfile: '../assets/profiles/creators/rick-astley-profile.png',
     creatorName: 'Rick Astley',
     subscriberCount: '4.21M',
     videoCount: '287'
   },
   {
     creatorId: 2,
+    creatorProfile: '../assets/profiles/creators/pets-island-profile.svg',
     creatorName: 'Pets Island',
     subscriberCount: '18.7K',
     videoCount: '333'
   },
   {
     creatorId: 3,
+    creatorProfile: '../assets/profiles/creators/kurzgesagt-profile.png',
     creatorName: 'Kurzgesagt',
     subscriberCount: '23.4M',
     videoCount: '264'
