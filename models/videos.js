672c2898cf449452b2077f9823ba3762a028b538
Isha Viragi <isha.viragi@gmail.com>
2024-12-29 17:10:17 -0500

    Update video descriptions to be more verbose

--- models/videos.js
+++ models/videos.js
@@ -3,7 +3,13 @@ export const videos = [
     videoId: 1,
     videoUrl: '../assets/videos/rick.png',
     title: 'Rick Astley - Never Gonna Give You Up',
-    description: `“Never Gonna Give You Up” was a global smash on its release in July 1987, topping the charts in 25 countries including Rick’s native UK and the US Billboard Hot 100.  It also won the Brit Award for Best single in 1988. Stock Aitken and Waterman wrote and produced the track which was the lead-off single and lead track from Rick’s debut LP “Whenever You Need Somebody”.  The album was itself a UK number one and would go on to sell over 15 million copies worldwide.`,
+    description: `The official video for “Never Gonna Give You Up” by Rick Astley. 
+    
+Never: The Autobiography 📚 OUT NOW! 
+Follow this link to get your copy and listen to Rick’s ‘Never’ playlist ❤️ #RickAstleyNever
+https://linktr.ee/rickastleynever
+    
+“Never Gonna Give You Up” was a global smash on its release in July 1987, topping the charts in 25 countries including Rick’s native UK and the US Billboard Hot 100.  It also won the Brit Award for Best single in 1988. Stock Aitken and Waterman wrote and produced the track which was the lead-off single and lead track from Rick’s debut LP “Whenever You Need Somebody”.  The album was itself a UK number one and would go on to sell over 15 million copies worldwide.`,
     uploadDate: new Date,
     creatorId: 1
   },
@@ -12,8 +18,33 @@ export const videos = [
     videoId: 1,
     videoUrl: '../assets/videos/cat.jpg',
     title: 'Cutest Cat Videos - Compilation',
-    description: `Welcome to the Pets Island where you can find lots of cute, funny and adorable videos about Pets. If you are an animal lover, you are in the right place. Because we will bring you daily videos of cutest Pets in the world. Are you ready for that?
-    Make sure to subscribe and hit the bell for daily updates. Pets Island is always waiting for you.`,
+    description: `We're celebrating International Cat Day by getting an adorably close look at the planet's former apex predator. Look at all these cute cats, failing like it's their job. We've got the best cat fails on the internet, so grab some catnip and buckle up... it's gonna be a fluffy ride.
+
+In honor of these adorable 
+
+►►► SUBMIT YOUR VIDEOS! http://bit.ly/fasubmit 
+SUBSCRIBE! http://bit.ly/fasubscribe
+FailArmy Merch Store • http://fail.army/2YU6Ax0
+
+▼ FOLLOW US FOR MORE FAILS!
+➤ Facebook ➝  http://bit.ly/FAILfacebook
+➤ Instagram ➝ http://bit.ly/FAILinstagram
+➤ Snapchat ➝  http://bit.ly/FAILsnapchat
+➤ TikTok ➝  http://bit.ly/FAILtiktok
+➤ Twitter ➝ http://bit.ly/FALtwitter
+
+Check out FailArmy U!!! • http://bit.ly/failu
+#failarmy #fails #cats
+
+We've got friends in high places! Give them a watch: 
+
+People Are Awesome -    / peopleareawesome  
+The Pet Collective -    / thepetcollective  
+This is Happening -    / thisishappening  
+
+FailArmy is the world’s number one source for epic fail videos and hilarious compilations. We’re powered by fan submissions and feedback from all around the world, with over 30 million fans across digital platforms! 
+
+To license any of the videos shown on FailArmy, please visit Jukin Media at http://bit.ly/jukinlicense.`,
     uploadDate: new Date,
     creatorId: 2
   },
@@ -22,7 +53,75 @@ export const videos = [
     videoId: 1,
     videoUrl: '../assets/videos/kurzgesagt.jpg',
     title: '4.5 Billion Years in 1 Hour',
-    description: `Earth is 4.5 billion years old - which is approximately the same amount of time it took us to create this video. We’ve scaled the complete timeline of our Earth’s life into our first animated movie! Every second shows about a million years of the planet’s evolution. Hop on a musical train ride and experience how long a billion years really is. It’s the perfect background for your next party, a great way to take a break from studying, or a fascinating companion while you’re on the go … and our celebration of 10 years of kurzgesagt.`,
+    description: `Learn more about how complex life evolved with our new, elaborately detailed Timeline of Evolution Poster. 
+Available only on the kurzgesagt shop here: https://shop.kgs.link/timeline
+
+Sources & further reading: https://sites.google.com/view/sources...
+
+Earth is 4.5 billion years old - which is approximately the same amount of time it took us to create this video.
+We’ve scaled the complete timeline of our Earth’s life into our first animated movie! Every second shows about a million years of the planet’s evolution. Hop on a musical train ride and experience how long a billion years really is. It’s the perfect background for your next party, a great way to take a break from studying, or a fascinating companion while you’re on the go … and our celebration of 10 years of kurzgesagt.
+
+OUR CHANNELS
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+German:        https://kgs.link/youtubeDE
+Spanish:        https://kgs.link/youtubeES
+French:          https://kgs.link/youtubeFR
+Portuguese:  https://kgs.link/youtubePT
+Arabic:           https://kgs.link/youtubeAR
+Hindi:             https://kgs.link/youtubeHI
+Japanese:     https://kgs.link/youtubeJA
+Korean:          https://kgs.link/youtubeKO
+
+
+HOW CAN YOU SUPPORT US?
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+This is how we make our living and it would be a pleasure if you support us!
+
+Get Products designed with ❤ https://shop-us.kurzgesagt.org  
+Join the Patreon Bird Army 🐧  https://kgs.link/patreon  
+
+
+DISCUSSIONS & SOCIAL MEDIA
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+TikTok:           https://kgs.link/tiktok
+Reddit:            https://kgs.link/reddit
+Instagram:     https://kgs.link/instagram
+Twitter:           https://kgs.link/twitter
+Facebook:      https://kgs.link/facebook
+Discord:          https://kgs.link/discord
+Newsletter:    https://kgs.link/newsletter
+
+
+OUR VOICE
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+The Kurzgesagt voice is from 
+Steve Taylor:  https://kgs.link/youtube-voice
+
+
+OUR MUSIC ♬♪
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+700+ minutes of Kurzgesagt Soundtracks by Epic Mountain:
+
+Spotify:            https://kgs.link/music-spotify
+Soundcloud:   https://kgs.link/music-soundcloud
+Bandcamp:     https://kgs.link/music-bandcamp
+Youtube:          https://bit.ly/49vGl2L
+Facebook:       https://kgs.link/music-facebook
+
+The Soundtrack of this video:
+Soundcloud: https://bit.ly/3u6XNdV
+Bandcamp: https://bit.ly/46bxD7i
+
+If you want to help us caption this video, please send subtitles to subtitle@kurzgesagt.org
+You can find info on what subtitle files work on YouTube here:
+https://support.google.com/youtube/an...
+Thank you!
+
+🐦🐧🐤 PATREON BIRD ARMY 🐤🐧🐦
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+Many Thanks to our wonderful Patreons (from http://kgs.link/patreon) who support us every month and made this video possible:
+robek, Brad Christensen, Asija Newns, Pedro Castro Verdezoto, Lucas Watkins, damien perolle, Fab Cars, alomanola, MikyF2K, Mithilesh Hinge, okor, Lucan, The Gnome, Kyung Geun Kim, Misa Brass, Martin Hippenstiel, Morgan, Anthony Distel, Filip Rogowski, Gabe, Tobias Hofmann, Zaret, Bootools, David Earl, Ben Hamilton, still [Redacted], polyploidy, Elgenzay, Jared, Soda celeste, Felix, Benjamin Goodrich, Taher Rangwala, Jesse Barnes, WillowTree, DarkKot 686, Aaron Phillips, Luc Royal, Lukas Meier, esamoth, Smithius, Mermor, Laura Kühne, Tyler Fendone, gassi30, Andres Arce, Suzanne frounfelter, Akito Nozaki, SirRender, Space, Daniel Hofmeister, Dan M, Jacob Culp, ReyesX77, Aaron T Haltman, DJ Dubbes, SchnarcheNoah, Keira Standing, Steven Lamontagne, Bliss, David Anderson, Uhh Uhhh Uhhh, The Paul Family
+`,
     uploadDate: new Date,
     creatorId: 3
   },
