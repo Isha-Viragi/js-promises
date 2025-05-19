cced4fa6dabe14953e2dbad7058f0186cbb4f316
Isha Viragi <isha.viragi@gmail.com>
2025-05-19 17:57:51 -0400

    feat(trendingVideos): get trending video

--- controllers/recommendVideos.js
+++ controllers/recommendVideos.js
@@ -5,3 +5,16 @@
 //Suggested videos
 //Trending videos
 //Random videos
+import { trendingVideos } from "../models/trendingVideos.js";
+
+function getTrendingVideos(milisecs, video) {
+  setTimeout(() => {
+    new Promise((resolve, reject) => {
+      const trendingVideosFound = trendingVideos.filter((v) => v.id !== video.id);
+
+      if (trendingVideosFound) resolve(trendingVideosFound);
+      else reject('Could not find trending videos')
+
+    })
+  }, milisecs);
+};
